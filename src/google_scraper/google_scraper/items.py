bf3bcf43245cca6352bbdcf4f907dee79cac0e93
Anna Wojcieszek <37207832+annopol@users.noreply.github.com>
2022-11-19 01:32:59 +0100

    Update items.py

--- src/google_scraper/google_scraper/items.py
+++ src/google_scraper/google_scraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class GoogleScraper3Item(scrapy.Item):
+class GoogleScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
